695b3febca7b39b2d4a84e865810d587b82d93b2
jollinho <joel.svenssson@live.se>
2015-04-05 13:21:17 -0700

    playlist start
    user can create playlist, view it while listening, can not yet add music
    to it.

---
+++ project/Models/PlayList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+namespace project.Models
+{
+    public class PlayList
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string PlaylistName { get; set; }
+        public string SongsID { get; set; }
+    }
+
+}
